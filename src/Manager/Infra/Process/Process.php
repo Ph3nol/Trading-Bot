df28ab446d3d200e4142068f35ca2b6999cc2e2c
Cédric Dugat <cedric@dugat.me>
2021-03-10 23:12:54 +0100

    Update

--- src/Manager/Infra/Process/Process.php
+++ src/Manager/Infra/Process/Process.php
@@ -4,6 +4,7 @@ namespace Manager\Infra\Process;
 
 use Symfony\Component\Process\Process as BaseProcess;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Exception\ProcessTimedOutException;
 
 /**
  * @author Cédric Dugat <cedric@dugat.me>
@@ -13,7 +14,13 @@ final class Process
     public static function processCommandLine(string $commandLine, bool $withException = true): ?string
     {
         $process = BaseProcess::fromShellCommandline($commandLine);
-        $process->run();
+        $process->setTimeout(120);
+
+        try {
+            $process->run();
+        } catch (ProcessTimedOutException $e) {
+            return null;
+        }
 
         if (!$process->isSuccessful() && $withException) {
             throw new ProcessFailedException($process);
